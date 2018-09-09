b6180c4d4c09efd10e8387166633554ff02f408e
Christian Soler <chr.solr@gmail.com>
2018-09-09 11:55:40 -0500

    Added eslint

--- configurations/settings.js
+++ configurations/settings.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const settings = {
-    server: {
-        DB_URL: process.env.DB_URL,
-        PORT: (process.env.PORT || 3000),
-        secrets: (process.env.SECRET || 'TOP_SECRET')
-    }
+  server: {
+    DB_URL: process.env.DB_URL,
+    PORT: (process.env.PORT || 3000),
+    secrets: (process.env.SECRET || 'TOP_SECRET')
+  }
 };
 
 module.exports = settings;
